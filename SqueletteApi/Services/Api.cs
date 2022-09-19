e04385712e091ffef16dbd515fdb12db926f6495
thierry le gall <thierry28220@live.fr>
2022-09-19 09:03:27 +0200

    Constantes

--- SqueletteApi/Services/Api.cs
+++ SqueletteApi/Services/Api.cs
@@ -15,10 +15,13 @@ namespace SqueletteApi.Services
         {
             try
             {
-                
+                //creation d'un navigateur
                 var clientHttp = new HttpClient();
+                //envoi d'une requete par URL et attente d'une reponse sous forme JSON
                 var json = await clientHttp.GetStringAsync(Constantes.BaseApiAddress + paramUrl);
+                //Conversion du JSON en objet
                 JsonConvert.DeserializeObject<List<T>>(json, new IsoDateTimeConverter { DateTimeFormat = "yyyy-MM-dd HH:mm:ss" });
+                //Retour sous forme de liste
                 return GestionCollection.GetListes<T>(param);
             }
             catch (Exception ex)
