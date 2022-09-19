a7a22ee51b792196ad8021479e91276ea4f3b488
thierry le gall <thierry28220@live.fr>
2022-09-19 14:03:24 +0200

    constantes

--- SqueletteApi/Constantes.cs
+++ SqueletteApi/Constantes.cs
@@ -10,5 +10,22 @@ namespace SqueletteApi
     {
         public static string BaseApiAddress => "http://172.17.0.61:8000/";
 
+        //gestion database
+
+        public const string DatabaseFilename = "Enchere2022.db3";
+
+        public const SQLite.SQLiteOpenFlags Flags =
+            SQLite.SQLiteOpenFlags.ReadWrite |
+            SQLite.SQLiteOpenFlags.Create |
+            SQLite.SQLiteOpenFlags.SharedCache;
+
+        public static string DatabasePath
+        {
+            get
+            {
+                var basePath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+                return Path.Combine(basePath, DatabaseFilename);
+            }
+        }
     }
 }
